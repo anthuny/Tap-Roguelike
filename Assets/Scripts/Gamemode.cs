0edbe18eb989e73f0ee18af9f3a1c59dcc55db75
anthuny <41554752+anthuny@users.noreply.github.com>
2020-06-26 22:42:09 +1000

    UI testing

---
+++ Assets/Scripts/Gamemode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemode : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Screen.fullScreen = true;
+
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
